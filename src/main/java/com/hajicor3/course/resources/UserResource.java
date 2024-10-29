63c83db87b9e213799f5e37eb177d9200063a09f
Gabriel Miranda <gabriel.mirandawv@outlook.com>
2024-10-28 22:30:37 -0300

    User entity and resource

---
+++ src/main/java/com/hajicor3/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.hajicor3.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hajicor3.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"maria","maria@gmail.com","99999999","12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
