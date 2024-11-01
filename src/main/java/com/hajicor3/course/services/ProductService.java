1e8967903c2920c22664f039842826caa793692b
Gabriel Miranda <gabriel.mirandawv@outlook.com>
2024-11-01 17:17:41 -0300

    product

---
+++ src/main/java/com/hajicor3/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.hajicor3.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hajicor3.course.entities.Product;
+import com.hajicor3.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+}
