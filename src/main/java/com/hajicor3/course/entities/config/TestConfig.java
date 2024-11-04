26bcca2479b14251b4bc772bb9875e8ee7ecd1cf
Gabriel Miranda <gabriel.mirandawv@outlook.com>
2024-11-03 21:51:23 -0300

    Payment one-to-one association

--- src/main/java/com/hajicor3/course/entities/config/TestConfig.java
+++ src/main/java/com/hajicor3/course/entities/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.hajicor3.course.entities.Category;
 import com.hajicor3.course.entities.Order;
 import com.hajicor3.course.entities.OrderItem;
+import com.hajicor3.course.entities.Payment;
 import com.hajicor3.course.entities.Product;
 import com.hajicor3.course.entities.User;
 import com.hajicor3.course.entities.enums.OrderStatus;
@@ -83,6 +84,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 	
